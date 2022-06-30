917c3f136c9117f67a5c48c51b79e4cfa524466d
Chris Loggins <75412063+clogginsdev@users.noreply.github.com>
2022-06-30 17:43:28 -0500

    Update ProductDetails.client.jsx

--- src/components/ProductDetails.client.jsx
+++ src/components/ProductDetails.client.jsx
@@ -182,7 +182,7 @@ function ProductGallery({ media }) {
         return (
           <div
             className={`${i % 3 === 0 ? "md:col-span-2" : "md:col-span-1"
-              } snap-center card-image bg-white aspect-square md:w-full w-[90vw] mr-2 shadow-sm rounded`}
+              } snap-center card-image bg-white aspect-square md:w-full w-[90vw] shadow-sm rounded`}
             key={med.id || med.image.id}
           >
             <MediaFile
