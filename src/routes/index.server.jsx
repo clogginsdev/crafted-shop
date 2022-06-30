00cd528a9f917cc4b78cf86cf80f35c7ff19ae88
Chris Loggins <75412063+clogginsdev@users.noreply.github.com>
2022-06-30 17:37:13 -0500

    Update index.server.jsx

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Suspense>
       <section className="px-8 mb-52 md:px-12">
         <h2 className="text-2xl mb-24 md:text-5xl">Discover our Summer Collection</h2>
-      <div className="grid grid-cols-1 md:gap-6 md:grid-cols-3 lg:gap-8">
+      <div className="grid grid-cols-1 gap-8 md:gap-6 md:grid-cols-3 lg:gap-8">
         
         {data.products.nodes.map((product) => {
           return (
